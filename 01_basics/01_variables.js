7563360054b3d79e440ea7ff72c67471e829f097
Asif Hussain <asifkhatian2@gmail.com>
2025-09-30 12:48:56 +0500

    First commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const id=444;
+let name="Asif";
+var age=22;
+account=104; //js also allow us without writing any keyword
+
+// id=2; not allowed
+
+name = "Asif Hussain";
+age=23;
+account=102
+
+console.table([name,age,account]);
