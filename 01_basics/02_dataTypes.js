d6243eb354f28170329e370129de731d0af0cecc
Asif Hussain <asifkhatian2@gmail.com>
2025-09-30 13:12:08 +0500

    Add initial data types demonstration in JavaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version but nowadays it automatically treat
+
+//alert(3+3); //only usable in browser
+
+console.log(3+3);
+console.log("Asif Hussain")
+
+
+let name = "Asif";
+let no =12;
+let isOlder = true;
+
+
+//number = 2 tot power 53
+//bigint 
+//string = ""
+//boolean = true/false
+//null = standalone value
+// undefined
+//symbol = unique
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
