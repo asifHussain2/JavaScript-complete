6842a99b758aabe3575cf8815640511bca10c541
Asif Hussain <asifkhatian2@gmail.com>
2025-09-30 16:02:39 +0500

    Refactor conversion operations and add additional examples in JavaScript

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -43,3 +43,40 @@ let d;
 console.log("undefined to number:",Number(d))
 console.log("undefined to String:",String(d))
 console.log("undefined to boolean:",Boolean(d))
+
+
+
+
+//Operations
+
+let value = 3;
+let negValue = -value;
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Asif";
+let str3 = str1+str2;
+console.log(str3)
+
+console.log("1" + 2);//12
+console.log(1+"2"); //12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log(+true);
+console.log(+"");
+
+let no1,no2,no3;
+
+no1=no2=no3=2+2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter)
