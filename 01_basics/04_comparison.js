a1003b276a9250d7d2fd6b0fd285d9f0c1b4161e
Asif Hussain <asifkhatian2@gmail.com>
2025-09-30 16:12:15 +0500

    Add comparison operations examples in JavaScript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0);//true
+
+// js comparison like > >= converts null to number while == doesnot convert null
+
+
+
+// === also checks datatype so avoid == always use === 
+
+console.log("2"===2);//false different datatype
