c1c3778ae017a9eacefbc19074d0138e31120230
Asif Hussain <asifkhatian2@gmail.com>
2025-10-01 19:38:19 +0500

    Refactor console log statements and enhance function examples in JavaScript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,10 +5,54 @@ if(true){
     
 let a =10;
 const b=20;
-console.log(a)
+// console.log(a)
 // var c = 30;
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Asif";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+
+}
+// one();
+
+
+if(true){
+    const username = "Asif";
+    if(username){
+        const website = " youtube"
+        // console.log(username+website);
+    }
+    // console.log(website) // not in scope
+}
+
+// console.log(username); // not in scope
+
+
+// interesting concept
+
+
+console.log(addOne(5));
+
+//function
+function addOne(num){
+    return num+1;
+}
+
+
+//expression
+var addTwo = function(num){ 
+    return num+2;
+}
+
+console.log(addTwo(5));
