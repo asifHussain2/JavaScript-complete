d202afa31a8568628d79dafa15636775ce68f8f7
Asif Hussain <asifkhatian2@gmail.com>
2025-10-01 19:12:16 +0500

    Refactor function logging and add examples for handling objects and arrays in JavaScript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,5 +29,30 @@ function loginUserMessage(username="Same"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Asif"))
+// console.log(loginUserMessage("Asif"))
 
+function calculateCartPrice(val1,val2,...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username:"Asif",
+    price:199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}.`)
+}
+
+
+handleObject(user)
+
+const myNewArray = [200,400,100,600];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray))
