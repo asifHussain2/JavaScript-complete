08f67e5cfb8d26558751af591222b4a22459188c
Asif Hussain <asifkhatian2@gmail.com>
2025-10-02 12:05:29 +0500

    Add while and do-while loop examples in JavaScript

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+
+const arr = ["Superman","Spiderman","Batman"];
+
+let index =0;
+while(index<arr.length){
+    console.log(arr[index]);
+    index++;
+}
+
+
+let score = 11;
+
+do{
+    console.log(score);
+    score++;
+}while(score<=10);
